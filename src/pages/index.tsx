89728c5aec9f907144371554d26f1cdb747055c5
lfernando <fernandocostadev98@gmail.com>
2023-04-05 14:40:48 -0300

    fix: remove Inter fonts from index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
 import { Heading } from "@chakra-ui/react";
-import { Inter } from "next/font/google";
-
-Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return <Heading>hello world</Heading>;
