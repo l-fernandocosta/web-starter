b52b51472af7fde3dd42894b3982cc6f3cc2e109
lfernando <fernandocostadev98@gmail.com>
2023-04-06 10:23:05 -0300

    chore: add useQuery example

--- src/utils/queries/pokemons/index.ts
+++ src/utils/queries/pokemons/index.ts
@@ -1,8 +1,8 @@
 import { GetAllPokemonsUseCase } from "@/@core/application/pokemons/get-all-pokemons.usecase";
 import { PokemonRegistry } from "@/@core/infra/registries/example.registry";
 import { container } from "@/@core/infra/registries/http.registry";
+import { QUERIES_KEYS } from "@/utils/queries/queries-key";
 import { useQuery } from "react-query";
-import { QUERIES_KEYS } from "../queries-key";
 
 export const get = async () => {
   const usecase = container.get<GetAllPokemonsUseCase>(
